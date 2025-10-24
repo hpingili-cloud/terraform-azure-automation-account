b87684aae2b899ab88e9f3a60a5cc5ac2fb41877
hpingili-cloud <pingiliharshith@outlook.com>
2025-10-24 14:51:48 -0700

    Create test.tf

---
+++ tests/test.tf
@@ -0,0 +1,16 @@
+module "automation_account" {
+  source  = "app.terraform.io/wcbbc/automation_account/azurerm"
+  version = "~> 0.0.2"
+
+  resource_group_name           = module.resource_group.name
+  tags                          = module.resource_group.tags
+  location                      = var.automation_account.location
+  name                          = var.automation_account.name
+  sku_name                      = var.automation_account.sku_name
+  public_network_access_enabled = true
+  enforce_naming_standard       = false
+  identity = {
+    type         = var.automation_account.identity.type
+    identity_ids = var.automation_account.identity.identity_ids
+  }
+}
